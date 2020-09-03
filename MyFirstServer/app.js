285f917b132bb3ddd09f21f727fec43031d95ada
Svante-J-TE <svante.jonsson@elev.ga.ntig.se>
2020-09-03 13:06:41 +0200

    created a static site

--- MyFirstServer/app.js
+++ MyFirstServer/app.js
@@ -1,1 +1,12 @@
-console.log("Hello Niklas")
+const express = require('express')
+const app = express()
+const port = 3000
+const clientDir = __dirname + "/client"
+
+app.get('/', (req, res) => res.sendFile(clientDir + '/index.html'))
+app.get('/style', (req, res) => res.sendFile(clientDir + '/style.css'))
+app.get('/IMG', (req, res) => {
+    res.sendFile(clientDir + '/joe_the_goat.jpg')
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
