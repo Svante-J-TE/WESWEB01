740e35896b5d4d8414bfcbdfee3b2e4a57aa69b7
Svante-J-TE <svante.jonsson@elev.ga.ntig.se>
2020-09-17 13:04:38 +0200

    yes

--- ejsTestMap/app.js
+++ ejsTestMap/app.js
@@ -5,7 +5,7 @@ const port = 3000
 
 app.set('view engine', 'ejs')
 
-let users = [{name: "Niklas", age: 16, color: "blue"}, {name: "Apa", age: 16}, {name: "Kung", age: 16} ]
+let users = [{name: "Niklas", age: 16, color: "blue"}, {name: "Apa", age: 16, color: "purple"}, {name: "Kung", age: 16, color: "red"} ]
 
 app.get('/', (req, res) => res.render(__dirname + '\\index.ejs', {users: users}))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
