a1ad82005d04ea1c55876032771d2f4e787bce9f
Svante-J-TE <54800689+Svante-J-TE@users.noreply.github.com>
2020-11-05 13:26:44 +0100

    login test, half working

---
+++ login_test/UserModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    password: String
+})
+
+const User = mongoose.model('User', userSchema);
+
+exports.createUser = (inName, inPassword) =>{
+    let user = new User({
+        name: inName,
+        password: inPassword
+    })
+    return user;
+}
+
+exports.findUsers = async () =>{
+    let user = await User.find({})
+    return user;
+  }
